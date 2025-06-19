df15d9c6fcee93e46b0d223a70e3224870328d46
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-06-19 20:05:06 +0300

    massively reworked formatters, implemented sotring logic in Xml formattter

--- cag/formatters/base.py
+++ cag/formatters/base.py
@@ -1,12 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from pathlib import Path
+
 from cag.core.base import Context
 
 
 class BaseCtxFormatter(ABC):
     """Interface, that defines methods to format ``Context``"""
 
+    def __init__(
+            self,
+            ctx: Context,
+    ):
+        self.ctx = ctx
+
     @abstractmethod
-    def __call__(self, context: Context) -> Any:
+    def __call__(self, *args, **kwargs) -> Any:
+        """Define formatting logic here."""
+        pass
+
+    @abstractmethod
+    def save(self, path: Path, name: str | None = None) -> None:
+        """Define storing logic here."""
         pass
--- cag/formatters/xml_format.py
+++ cag/formatters/xml_format.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
+from pathlib import Path
+
 from .base import BaseCtxFormatter
 from ..core.base import Context
 
@@ -8,13 +10,20 @@ from ..core.base import Context
 class XMLCtxFormatter(BaseCtxFormatter):
     """Formats ``Context`` into unified XML, text file."""
 
-    def __call__(self, context: Context) -> str:
+    def __init__(
+            self,
+            ctx: Context,
+    ):
+        super().__init__(ctx)
+        self.format_ctx = None
+
+    def __call__(self) -> str:
         root = ET.Element(
             'Context',
-            attrib=self._serialize_metadata(context.metadata)
+            attrib=self._serialize_metadata(self.ctx.metadata)
         )
 
-        for unit in context.ctx_units:
+        for unit in self.ctx.ctx_units:
             elem = ET.SubElement(
                 root,
                 'ContextUnit',
@@ -23,9 +32,9 @@ class XMLCtxFormatter(BaseCtxFormatter):
             elem.text = unit.content
 
         xlm_str = ET.tostring(root)
-        format_xml = minidom.parseString(xlm_str).toprettyxml()
+        self.format_ctx = minidom.parseString(xlm_str).toprettyxml()
 
-        return format_xml
+        return self.format_ctx
 
     @staticmethod
     def _serialize_metadata(metadata: dict) -> dict:
@@ -33,3 +42,14 @@ class XMLCtxFormatter(BaseCtxFormatter):
             metadata[key] = str(val)
 
         return metadata
+
+    def save(self, path: Path, name: str | None = None) -> None:
+        """
+        :param name: Storing ``Context`` name without extension.
+        :param path: Storing directory.
+        """
+        if not name:
+            name = self.ctx.metadata.get('name')
+
+        with open(path.joinpath(name + '.xlm'), 'w') as file:
+            file.write(self.format_ctx)
